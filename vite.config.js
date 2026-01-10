533418200e417f4875a8e3d440832356f9f80781
achraf <achraf04chke@gmail.com>
2026-01-10 03:01:37 +0100

    add

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Important pour que les chemins soient relatifs
+  base: '.',  // Important pour que les chemins soient relatifs
 })
