0cc46bd7aad2142caa922c588168d75c8707dfa4
achraf <achraf04chke@gmail.com>
2026-01-10 14:57:48 +0100

    fix: add main.jsx script to index.html to resolve blank page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite'; 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // chemins relatifs
-})
+   base: '/<Portfolio-website/',  // Remplacez <nom-du-repo> par le nom exact de votre dépôt GitHub
+  });  
 
