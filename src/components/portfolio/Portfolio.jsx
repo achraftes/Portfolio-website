f87aaf56e7d3ef402344a860bccf4201b67805ec
achraf <achraf04chke@gmail.com>
2026-01-10 17:40:24 +0100

    update  code

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
     setToggleState(index);
   };
 
-  // Ajouter les liens GitHub ici
+  // Liens GitHub pour chaque projet
   const githubLinks = {
     1: "https://github.com/YourUsername/ecommerce-site",
     2: "https://github.com/YourUsername/weather-app",
@@ -16,7 +16,7 @@ const Portfolio = () => {
     4: "https://github.com/YourUsername/website-management",
     5: "https://github.com/YourUsername/jwt-project",
     6: "https://github.com/YourUsername/autodata-manager",
-    7: "https://github.com/achraftes/EcomAgentManager-APP"
+    7: "https://github.com/YourUsername/ecomagentmanager-app"
   };
 
   return (
@@ -25,45 +25,49 @@ const Portfolio = () => {
       <span className="section__subtitle">Most recent work</span>
 
       <div className="portfolio__container container grid">
-        {/* Project 1 */}
+
+        {/* ===== Projet 1 ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">E-commerce Site</h3>
+            <a href={githubLinks[1]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(1)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[1]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 1 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
               <i onClick={() => toggleTab(0)} className="uil uil-times portfolio__modal-close"></i>
-              <h3 className="portfolio__modal-title">E-commerce site</h3>
+              <h3 className="portfolio__modal-title">E-commerce site </h3>
               <p className="portfolio__modal-description">
-                This project is an online sales platform including a home page,
-                a contact page, a product page, and a product detail page. Developed with React for dynamic structure and navigation, 
-                it uses HTML and CSS for a simple, clear, and responsive design. Each page highlights a smooth user experience, 
-                with well-organized product details and an intuitive interface. The project is a demonstration of my React and web design skills.
+                 This project is an online sales platform including a home page,
+                  a contact page, a product page, and a product detail page. 
+                  Developed with React for dynamic structure and navigation, 
+                  it uses HTML and CSS for a simple, clear, and responsive design. 
+                  Each page highlights a smooth user experience, with well-organized 
+                  product details and an intuitive interface. The project is a demonstration
+                   of my React and web design skills.
               </p>
             </div>
           </div>
         </div>
 
-        {/* Project 2 */}
+        {/* ===== Projet 2 ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">Weather App</h3>
+            <a href={githubLinks[2]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(2)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[2]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 2 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
@@ -71,26 +75,27 @@ const Portfolio = () => {
               <h3 className="portfolio__modal-title">Interactive weather app</h3>
               <p className="portfolio__modal-description">
                 This app allows users to check the weather by entering their country and city name.
-                Developed with React, HTML and CSS, it uses Axios to consume a weather API and display accurate information. 
-                The project integrates several React hooks to manage data, state and effects, providing a fluid and dynamic interface. 
-                The application stands out for its ease of use and its ability to provide results in real time.
+                Developed with React, HTML and CSS, it uses Axios to consume a weather API and display
+                accurate information. The project integrates several React hooks to manage data, state
+                and effects, providing a fluid and dynamic interface. The application stands out for 
+                its ease of use and its ability to provide results in real time.
               </p>
             </div>
           </div>
         </div>
 
-        {/* Project 3 */}
+        {/* ===== Projet 3 ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">To-Do List App</h3>
+            <a href={githubLinks[3]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(3)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[3]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 3 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
@@ -98,96 +103,120 @@ const Portfolio = () => {
               <h3 className="portfolio__modal-title">To-Do List application for management</h3>
               <p className="portfolio__modal-description">
                 This project is a Full-Stack application designed to manage administrative tasks for schools.
-                Backend: Laravel with REST APIs. Frontend: React with Axios. MySQL database.
+                The backend part was developed with Laravel, where all RESTful methods were implemented to 
+                allow complete task management, including creation, modification, deletion and recovery of data.
+                The data is stored in a MySQL database, ensuring an efficient and secure structure.
+                On the frontend side, the app was built with React, offering a modern and intuitive user interface.
+                Data is retrieved and manipulated in real time using Axios, which consumes REST APIs created with Laravel.
+                The design is simple but effective, suitable for smooth and pleasant use.
+                This project illustrates a perfect integration between backend and frontend technologies,
+                demonstrating mastery of Laravel, React, MySQL, and API calls.
               </p>
             </div>
           </div>
         </div>
 
-        {/* Project 4 */}
+        {/* ===== Projet 4 ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">Website Management</h3>
+            <a href={githubLinks[4]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(4)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[4]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 4 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
               <i onClick={() => toggleTab(0)} className="uil uil-times portfolio__modal-close"></i>
               <h3 className="portfolio__modal-title">Website for business management</h3>
               <p className="portfolio__modal-description">
-                Full-Stack website developed with Laravel for inventory, sales, purchases, and financial reporting.
+                This project is a Full-Stack website developed with Laravel for inventory management,
+                sales, purchases and financial reporting. It includes pages like homepage, services page, 
+                contact page, and an admin section for product and order management. The site is offered as a
+                SaaS, accessible from any device with an Internet connection, and allows store owners to monitor 
+                and optimize their business operations in real time.
               </p>
             </div>
           </div>
         </div>
 
-        {/* Project 5 */}
+        {/* ===== Projet 5 ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">JWT</h3>
+            <a href={githubLinks[5]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(5)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[5]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 5 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
               <i onClick={() => toggleTab(0)} className="uil uil-times portfolio__modal-close"></i>
               <h3 className="portfolio__modal-title">JWT</h3>
               <p className="portfolio__modal-description">
-                Full-Stack app with Node.js & MongoDB using JWT for secure authentication and REST API for product & order management.
+                  This project is a Full-Stack application developed with Node.js and MongoDB,
+                  allowing the management of products and orders. User authentication is carried
+                  out via JSON Web Tokens (JWT), ensuring secure access to the various functionalities.
+                  Users can register, log in and manage products as well as orders. The backend is based 
+                  on REST APIs developed with Express.js. In addition, extensive testing of the APIs was 
+                  carried out with Postman to ensure they function properly and are reliable. 
+                  This project offers a robust solution for product management platforms or inventory 
+                  management systems.
               </p>
             </div>
           </div>
         </div>
 
-        {/* Project 6 */}
+        {/* ===== Projet 6 ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">AutoData Manager</h3>
+            <a href={githubLinks[6]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(6)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[6]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 6 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
               <i onClick={() => toggleTab(0)} className="uil uil-times portfolio__modal-close"></i>
               <h3 className="portfolio__modal-title">AutoData Manager</h3>
               <p className="portfolio__modal-description">
-                Python Tkinter app for managing vehicle information stored in a local file. Simple, intuitive UI.
+                  This project is a system developed with Python and the Tkinter library
+                  to create a simple and intuitive graphical interface. The program allows
+                  the user to enter information about a vehicle, such as make, model, year,
+                  mileage, color, and other important details. This data is then stored in
+                  a local file on the computer, providing convenient and centralized information 
+                  management. The interface is easy to use, allowing for quick and efficient data 
+                  entry without requiring an external database.
               </p>
             </div>
           </div>
         </div>
 
-        {/* Project 7 - EcomAgentManager-APP */}
+        {/* ===== Projet 7 ===== EcomAgentManager-APP ===== */}
         <div className="portfolio__content">
-          <div>
+          <div className="portfolio__header">
             <i className="bx bxs-folder-open portfolio__icon"></i>
             <h3 className="portfolio__title">EcomAgentManager-APP</h3>
+            <a href={githubLinks[7]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
+              <i className="bx bxl-github"></i>
+            </a>
           </div>
           <span className="portfolio__button" onClick={() => toggleTab(7)}>
             View More <i className="uil uil-arrow-right portfolio__button-icon"></i>
           </span>
-          <a href={githubLinks[7]} target="_blank" rel="noopener noreferrer" className="portfolio__github">
-            <i className="bx bxl-github"></i>
-          </a>
 
           <div className={toggleState === 7 ? "portfolio__modal active-modal" : "portfolio__modal"}>
             <div className="portfolio__modal-content">
