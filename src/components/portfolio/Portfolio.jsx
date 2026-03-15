634b043c020184046f5bebf0f1ccc4731b23eb21
achraf <achraf04chke@gmail.com>
2026-03-15 16:30:30 +0000

    add  new lik fur github

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -25,7 +25,7 @@ const Portfolio = () => {
             </span>
           </div>
           <div className="portfolio__footer">
-            <a href="https://github.com/yourusername/ecommerce" target="_blank" rel="noopener noreferrer" className="portfolio__github">
+            <a href="https://github.com/achraftes/Ecommerce-app" target="_blank" rel="noopener noreferrer" className="portfolio__github">
               <i className="bx bxl-github"></i>
             </a>
           </div>
