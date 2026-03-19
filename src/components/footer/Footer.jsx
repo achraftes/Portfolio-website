73252a54e0968ab780cc6ebc2044b1bdcc1e9fe2
achraf <achraf04chke@gmail.com>
2026-03-19 18:41:39 +0000

    feat(footer): redesign layout with 3-column grid structure

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,43 +1,58 @@
-import React from 'react'
-import "./footer.css"
+import React from 'react';
+import "./footer.css";
+
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer__container container">
-                <h1 className="footer__title">Chikrabane</h1>
+  const currentYear = new Date().getFullYear();
 
-                <ul className="footer__list">
-                    <li>
-                        <a href="#about" className="footer__link">About</a>
-                    </li>
-                    
-                    <li>
-                        <a href="#portfolio" className="footer__link">Projects</a>
-                    </li>
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
 
-                    <li>
-                        <a href="#services" className="footer__link">Services</a>
-                    </li>
-                </ul>
+        <div className="footer__top">
+          <div className="footer__brand">
+            <h1 className="footer__title">Chikrabane</h1>
+            <p className="footer__tagline">Full-Stack Developer & Designer</p>
+          </div>
 
-                <div className="footer__social">
-                <a href="https://www.instagram.com/achraf_chkr/" className="footer__social-link" target="_blank">
-                   <i className="bx bxl-instagram"></i>
-                </a>
+          <nav className="footer__nav">
+            <span className="footer__nav-label">Navigation</span>
+            <ul className="footer__list">
+              <li><a href="#about" className="footer__link">About</a></li>
+              <li><a href="#portfolio" className="footer__link">Projects</a></li>
+              <li><a href="#services" className="footer__link">Services</a></li>
+            </ul>
+          </nav>
 
-                <a href="https://www.linkedin.com/in/achraf-chikrabane-850554291/" className="footer__social-link" target="_blank">
-                 <i className="bx bxl-linkedin" ></i>
-                 </a>
-
-                 <a href="https://x.com/chkr_achraf" className="footer__social-link" target="_blank">
-                   <i className="bx bxl-twitter"></i>   
-                  </a>
+          <div className="footer__contact-block">
+            <span className="footer__nav-label">Connect</span>
+            <div className="footer__social">
+              <a href="https://www.instagram.com/achraf_chkr/" className="footer__social-link" target="_blank" rel="noreferrer" aria-label="Instagram">
+                <i className="bx bxl-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/achraf-chikrabane-850554291/" className="footer__social-link" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+                <i className="bx bxl-linkedin"></i>
+              </a>
+              <a href="https://x.com/chkr_achraf" className="footer__social-link" target="_blank" rel="noreferrer" aria-label="Twitter / X">
+                <i className="bx bxl-twitter"></i>
+              </a>
             </div>
-
-            <span className="footer__copy">© {new Date().getFullYear()} Chikrabane. All rigths reserved</span>
+          </div>
         </div>
-     </footer>
-    )
-}
 
-export default Footer
+        <div className="footer__divider"></div>
+
+        <div className="footer__bottom">
+          <span className="footer__copy">
+            © {currentYear} Achraf Chikrabane. All rights reserved.
+          </span>
+          <span className="footer__made">
+            Designed & built with <span className="footer__heart">♥</span> by Achraf
+          </span>
+        </div>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
